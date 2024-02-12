ea9053f8de5b197bf6ad4ff32c78ee452ee9c962
Rockas888 <105117889+Rockas888@users.noreply.github.com>
2024-02-13 00:27:03 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,6 +2,6 @@ api_id = 24508138
 api_hash = "94bf6b17836bedb54c4c3ab0aaf26eaf"
 bot_token = "6594535402:AAHMX-9zO4JIVwe7_DLbrHNyJ0VC87ibflM"
 auth_users = [6585878012]
-sudo_user = 6585878012
+sudo_user = -1001915281770
 log_channel = -1002040712606
 txt_channel = -1001915281770
