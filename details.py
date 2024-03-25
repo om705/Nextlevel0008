122313e68b94cdea34b909e9c58344b99fdcd399
om705 <61368509+om705@users.noreply.github.com>
2024-03-25 21:30:19 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,6 +1,7 @@
-api_id = 24508138
-api_hash = "94bf6b17836bedb54c4c3ab0aaf26eaf"
-bot_token = "6487209013:AAGeMS4qWaz5mh4xKeni9diyeCuQ8gg_-6g"
+api_id = 28245628
+api_hash = "
+42b209c25a21cb19c0b80bdb8da899a4"
+bot_token = "6718023700:AAGqRIBG-1JrJAC8xtpgmnMsdSvjhry-GT8"
 auth_users = [6680176343,6156489363,-1001942973034]
 sudo_user = 6680176343
 log_channel = -1001834213725
